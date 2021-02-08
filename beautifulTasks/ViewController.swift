39d95b357597ca52711c9a9335160c2bf8c62729
Joao Silva <josejoaosilva.n@gmail.com>
2021-02-08 09:40:23 -0300

    first commit

---
+++ beautifulTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beautifulTasks
+//
+//  Created by José João Silva Nunes Alves on 08/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
